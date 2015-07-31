fe080ff731a2adb1cb54ed2b4e1a04de969697cc
dhavalsinhzala <dhaval.zala@live.com>
2015-07-31 17:30:48 +0530

    some changes

--- src/js/dominoes.js
+++ src/js/dominoes.js
@@ -3,21 +3,18 @@
 	app.controller("MainController", [ '$scope', function($scope) {
 		$scope.indexes = [];
 		$scope.list = [];
-
 		$scope.find = function(inputText) {
 			var sList = new linkedList(), dataItems = inputText.split(" ");
-
 			for (var index = 0; index < dataItems.length; index++) {
 				if (dataItems[index] !== "") {
 					sList.add(dataItems[index]);
 				}
 			}
-			for(index = 0; index < 10; index++){
+			for (index = 0; index < 10; index++) {
 				var result = sList.getIndexesSeries(index);
 				$scope.indexes[index] = result;
 			}
-			
-			$scope.results =  sList.findSeries();
+			$scope.results = sList.findSeries();
 		};
 	} ]);
 })();
--- src/js/linkedlist.js
+++ src/js/linkedlist.js
@@ -1,96 +1,37 @@
 function linkedList() {
-	var list = {}, start = null, end = null,resultsStart = null, resultsEnd= null, indexes = [], endIndexes = [];
-
+	var list = {}, start = null, end = null, resultsStart = null, resultsEnd = null, indexes = [], endIndexes = [];
 	function makeNode(data) {
 		return {
-			data: data,
-			isAvailable: true,
+			data : data,
+			isAvailable : true,
 			next : null
 		};
 	}
-
 	function makeIndexNode(node) {
 		return {
-			node: node,
+			node : node,
 			next : null
 		};
 	}
-	
-	function makeResultNode(value1, value2){
+	function makeResultNode(value1, value2) {
 		return {
-			value1: value1,
-			value2: value2,
-			next: null,
-			prev: null
+			value1 : value1,
+			value2 : value2,
+			next : null,
+			prev : null
 		};
 	}
-
-	function addIndex(index, node){
-		var indexNode = makeIndexNode(node); 
-		if(indexes[index]){
+	function addIndex(index, node) {
+		var indexNode = makeIndexNode(node);
+		if (indexes[index]) {
 			endIndexes[index].next = indexNode;
 			endIndexes[index] = indexNode;
-		}
-		else{
+		} else {
 			indexes[index] = indexNode;
 			endIndexes[index] = indexNode;
 		}
 	}
-
-	list.add = function(data) {
-		if(data && data.length === 2){
-			var values = data.split("");
-			if(!isNaN(values[0]) && !isNaN(values[1]) && ( values[0] !== values[1]))
-			{
-				var node = makeNode(data);
-				if (start === null) {
-					start = node;
-					end = node;
-				} else {
-					end.next = node;
-					end = node;
-				}
-
-				addIndex(values[0], node);
-				addIndex(values[1], node);
-			}
-		}
-	};
-	
-	list.getIndexesSeries = function(index){
-		var save = indexes[index], results = [];;
-		while (save != null) {
-			results.push(save.node.data);
-			save = save.next;
-		}
-		
-		return results;
-	}
-	
-	list.findSeries = function(){
-		var currentNode = null, index =0, value1, value2, resultNode;
-		while(indexes[index] === undefined){
-			index++;
-			if(index == 10){
-				return;
-			}
-		}
-		
-		currentNode = indexes[index];
-		value1 = index;
-		
-		while(currentNode){
-			value2 = currentNode.node.data.replace(value1,"").split("")[0];
-			currentNode.node.isAvailable = false;
-			addResultNode(value1, value2);
-			currentNode = findNextAvailbleIndexNode(value2);
-			value1 = value2;
-		}
-		
-		return getResultsList();
-	}
-	
-	function addResultNode(value1,value2){
+	function addResultNode(value1, value2) {
 		var node = makeResultNode(value1, value2);
 		if (resultsStart === null) {
 			resultsStart = node;
@@ -100,27 +41,65 @@ function linkedList() {
 			resultsEnd = node;
 		}
 	}
-	
-	function getResultsList(){
-		var save = resultsStart, results = [];;
+	function getResultsList() {
+		var save = resultsStart, results = [];
 		while (save != null) {
 			results.push(save.value1 + save.value2);
 			save = save.next;
 		}
-		
 		return results;
 	}
-	
-	function findNextAvailbleIndexNode(index){
+	function findNextAvailbleIndexNode(index) {
 		var save = indexes[index];
 		while (save != null && !save.node.isAvailable) {
 			save = save.next;
 		}
 		return save;
 	}
-	
-	list.remove = function() {
-
+	list.add = function(data) {
+		if (data && data.length === 2) {
+			var values = data.split("");
+			if (!isNaN(values[0]) && !isNaN(values[1])
+					&& (values[0] !== values[1])) {
+				var node = makeNode(data);
+				if (start === null) {
+					start = node;
+					end = node;
+				} else {
+					end.next = node;
+					end = node;
+				}
+				addIndex(values[0], node);
+				addIndex(values[1], node);
+			}
+		}
+	};
+	list.getIndexesSeries = function(index) {
+		var save = indexes[index], results = [];
+		while (save != null) {
+			results.push(save.node.data);
+			save = save.next;
+		}
+		return results;
+	};
+	list.findSeries = function() {
+		var currentNode = null, index = 0, value1, value2;
+		while (indexes[index] === undefined) {
+			index++;
+			if (index === 10) {
+				return;
+			}
+		}
+		currentNode = indexes[index];
+		value1 = index;
+		while (currentNode) {
+			value2 = currentNode.node.data.replace(value1, "").split("")[0];
+			currentNode.node.isAvailable = false;
+			addResultNode(value1, value2);
+			currentNode = findNextAvailbleIndexNode(value2);
+			value1 = value2;
+		}
+		return getResultsList();
 	};
 	return list;
 }
